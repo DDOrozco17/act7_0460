4eb1f1ef6ff6dd453c81eca68a8f4ef850facbb7
DIEGO_DIAZ_OROZCO <a.21308051280460@cbtis128.edu.mx>
2024-05-17 16:51:17 +0000

    act

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,36 +9,98 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: 'Papeleria Diego 0460',
       theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
+        scaffoldBackgroundColor:
+            const Color(0xfffffdd0), // Color de fondo amarillo claro
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> imgList = [
+    "https://raw.githubusercontent.com/DDOrozco17/Img_FlutterFlow_IOS_6J/main/ImgDiaz/CuadernosArgollados.png",
+    "https://raw.githubusercontent.com/DDOrozco17/Img_FlutterFlow_IOS_6J/main/ImgDiaz/CuadernosGrapados.png",
+    "https://raw.githubusercontent.com/DDOrozco17/Img_FlutterFlow_IOS_6J/main/ImgDiaz/Folder.png",
+    "https://raw.githubusercontent.com/DDOrozco17/Img_FlutterFlow_IOS_6J/main/ImgDiaz/MarcadoresVinilo.png",
+    "https://raw.githubusercontent.com/DDOrozco17/Img_FlutterFlow_IOS_6J/main/ImgDiaz/PapelImpresora.png",
+  ];
+
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xfffffdd0), // Color de fondo amarillo claro
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        backgroundColor:
+            const Color(0xffd4af37), // Color de la barra de aplicación dorado
+        title: const Text(
+          "Papeleria Diego 0460",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: Column(
+          children: [
+            CarouselSlider(
+              items: imgList
+                  .map((e) => Center(
+                        child: Image.network(e),
+                      ))
+                  .toList(),
+              options: CarouselOptions(
+                  initialPage: 0,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 2),
+                  enlargeCenterPage: true,
+                  enlargeFactor: 0.3,
+                  onPageChanged: (value, _) {
+                    setState(() {
+                      _currentPage = value;
+                    });
+                  }),
+            ),
+            buildCarouselIndicator(),
+            const Text(
+              "Prueba nuestra variedad de colores",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.black), // Texto en negro
+            )
+          ],
         ),
       ),
     );
+  }
+
+  buildCarouselIndicator() {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      for (int i = 0; i < imgList.length; i++)
+        Container(
+          margin: const EdgeInsets.all(5),
+          height: i == _currentPage ? 7 : 5,
+          width: i == _currentPage ? 7 : 5,
+          decoration: BoxDecoration(
+              color: i == _currentPage
+                  ? const Color(
+                      0xffffffff) // Color del indicador de la página seleccionada blanco
+                  : const Color(
+                      0xffefd807), // Color del indicador de la página no seleccionada dorado claro
+              shape: BoxShape.circle),
+        )
+    ]);
   }
 }
